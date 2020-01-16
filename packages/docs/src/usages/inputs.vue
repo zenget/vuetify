7b9c21e1237c2108e5f32d8a7a4856315f6b6187
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2020-01-16 19:10:32 +0300

    docs(VInput): refactor usage and playground

---
+++ packages/docs/src/usages/inputs.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container class="fill-height">
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-input
+        v-bind="attrs"
+        :messages="['Messages']"
+        :success-messages="attrs.success ? ['Done!'] : []"
+        :error-messages="attrs.error ? ['Error', 'Another one', 'One more', 'All the errors'] : []"
+        hint="I am hint"
+        append-icon="close"
+        prepend-icon="phone"
+        @click:append="appendIconCallback"
+        @click:prepend="prependIconCallback"
+      >
+        Default Slot
+      </v-input>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import Usage from './usage'
+
+  export default {
+    mixins: [Usage],
+    methods: {
+      appendIconCallback () {
+        alert('click:append')
+      },
+      prependIconCallback () {
+        alert('click:prepend')
+      },
+    },
+  }
+</script>
