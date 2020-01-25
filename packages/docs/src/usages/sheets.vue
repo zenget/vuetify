63a4dc74552a97c93b0b5f13d467c66c90b0da47
Yiping <Yiping.Ruan@outlook.com>
2020-01-25 20:33:46 +0800

    Sheets booleans

--- packages/docs/src/usages/sheets.vue
+++ packages/docs/src/usages/sheets.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-calendar v-bind="attrs" />
+  <v-sheet
+    :width="width"
+    :height="height"
+    :elevation="elevation"
+    v-bind="attrs"
+  />
 </template>
 
 <script>
@@ -7,5 +12,10 @@
 
   export default {
     mixins: [Usage],
+    data: () => ({
+      width: 100,
+      height: 100,
+      elevation: 4,
+    }),
   }
 </script>
