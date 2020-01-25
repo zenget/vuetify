3d03e878897823b4c8920799cf857619fdf4b5e4
Yiping <Yiping.Ruan@outlook.com>
2020-01-25 22:08:54 +0800

    Snackbars booleans

--- packages/docs/src/usages/snackbars.vue
+++ packages/docs/src/usages/snackbars.vue
@@ -1,5 +1,28 @@
 <template>
-  <v-calendar v-bind="attrs" />
+  <div>
+    <v-btn
+      block
+      color="primary"
+      dark
+      @click="snackbar = true"
+    >
+      Show Snackbar
+    </v-btn>
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="timeout"
+      v-bind="attrs"
+    >
+      {{ text }}
+      <v-btn
+        dark
+        text
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
@@ -7,5 +30,12 @@
 
   export default {
     mixins: [Usage],
+    data () {
+      return {
+        snackbar: false,
+        text: 'Hello, I\'m a snackbar',
+        timeout: 6000,
+      }
+    },
   }
 </script>
